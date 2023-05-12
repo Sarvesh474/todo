6af9c4a98d77eee3d686b1a91243a12e289fc1f5
Sarvesh Sharma <s.sharma39@outlook.com>
2023-05-12 15:54:20 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [activity,setActivity]=useState("");
+const [list,setList]=useState([])
+
+const handleClick=()=>{
+  // setList([...list,activity])
+  // console.log(list)
+  setList(list=>{
+    const res=[...list,activity]
+    console.log(res)
+    setActivity("")
+    return res;
+  })
+}
+
+const handleDelete=(i)=>{
+  const res=list.filter((ele,id)=>{
+    return i!=id
+  })
+  setList(res)
+ 
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+		<h1>Todo List</h1>
+		<div className="todo-input">
+			<input 
+      type="text" 
+      placeholder="Enter task"
+      value={activity}
+      onChange={(e)=>setActivity(e.target.value)}
+      />
+			<button onClick={handleClick}>Add</button>
+		</div>
+    <ul className='todo-list'>
+      {list!=[] && list.map((data,i)=>{
+        return(
+          <>
+				<li key={i} className="todo-item">
+          <div  >{data}</div>
+          <div><button onClick={()=>handleDelete(i)}>Delete</button></div>
+        </li>
+				
+        </>
+        )
+        
+      })}
+    
+    </ul>
     </div>
   );
 }
